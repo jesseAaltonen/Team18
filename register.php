522db69532523d475cc9ba1d3f3ab5032f566c98
jesseAaltonen <jesse.aaltonen48@gmail.com>
2023-02-10 11:27:57 +0200

    register.php

--- register.php
+++ register.php
@@ -1,11 +1,27 @@
 <?php
 $email = isset($_post["email"]) ? $_POST["email"] : "";
-$email = isset($_post["email"]) ? $_POST["email"] : "";
+$password = isset($_post["password"]) ? $_POST["password"] : "";
+mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
+try{
+    $yhteys=mysqli_connect("db", "root", "password", "team18");
+}
+catch(Exception $e){
+    header("Location:../html/yhteysvirhe.html");
+    exit;
+}
 
+if(!empty($email) && !empty($password)){
+    $sql="insert into user (email, password) values(?, ?)";
+    //Valmistellaan sql-lause
+    $stmt=mysqli_prepare($yhteys, $sql);
+    //Sijoitetaan muuttujat oikeisiin paikkoihin
+    mysqli_stmt_bind_param($stmt, 'ss', $email, $password);
+    //Suoritetaan sql-lause
+    mysqli_stmt_execute($stmt);
+    }
 
-
-
-
-
+    //Suljetaan tietokantayhteys
+    mysqli_close($yhteys);
+   
 ?>
