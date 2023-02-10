ef826a3ea7a44396580d3bf834d948642f99ff0c
SauliKaloinen <sauli1603@student.hamk.fi>
2023-02-10 11:38:37 +0200

    fsasd

--- register.php
+++ register.php
@@ -11,15 +11,23 @@ catch(Exception $e){
     exit;
 }
 
-if(!empty($email) && !empty($password)){
-    $sql="insert into user (email, password) values(?, ?)";
-    //Valmistellaan sql-lause
-    $stmt=mysqli_prepare($yhteys, $sql);
-    //Sijoitetaan muuttujat oikeisiin paikkoihin
-    mysqli_stmt_bind_param($stmt, 'ss', $email, $password);
-    //Suoritetaan sql-lause
-    mysqli_stmt_execute($stmt);
+$last_id = mysqli_insert_id($yhteys);
+
+    foreach ($email as $password ){
+        $sql="insert into user(email, password) values(?, ?)";
+        $stmt=mysqli_prepare($yhteys, $sql);
+        mysqli_stmt_bind_param($stmt, 'ss', $email, $password);
+        mysqli_stmt_execute($stmt);
     }
+
+    header("Location:tallennahenkilo.php");
+    exit;
+
+
+
+
+    
+    
 
     //Suljetaan tietokantayhteys
     mysqli_close($yhteys);
